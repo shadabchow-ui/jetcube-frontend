8e26c7f81631a6b27d4546c5943dd9e2b6d5f689
shadabchow-ui <shadabchow@gmail.com>
2026-01-14 08:07:10 -0500

    Create CategoryDirectory.tsx

---
+++ src/screens/CategoryDirectory.tsx
@@ -0,0 +1,14 @@
+export default function CategoryDirectory() {
+  return (
+    <iframe
+      src="/category-directory/index.html"
+      title="Category Directory"
+      style={{
+        width: '100%',
+        height: '100vh',
+        border: 'none',
+        background: '#0b0f19',
+      }}
+    />
+  );
+}
