c062e52962ed33f4dcc0b0af6f584708b20f9b12
shadabchow-ui <shadabchow@gmail.com>
2026-02-15 10:57:15 -0500

    Update ProductComparison.tsx

--- src/screens/ProductComparison/ProductComparison.tsx
+++ src/screens/ProductComparison/ProductComparison.tsx
@@ -1,4 +1,4 @@
-import { ShopNavigation } from "../../screens/Shop/components/ShopNavigation";
+import NavigationSection from "../Shop/sections/NavigationSection/NavigationSection";
 import { ProductComparisonSection } from "./sections/ProductComparisonSection";
 import { QualityAssuranceSection } from "./sections/QualityAssuranceSection";
 import { RelatedProductsSection } from "./sections/RelatedProductsSection";
@@ -7,7 +7,7 @@ export const ProductComparison = (): JSX.Element => {
   return (
     <div className="product-comparison">
       <div className="div-9">
-        <ShopNavigation />
+        <NavigationSection />
         <ProductComparisonSection />
         <QualityAssuranceSection />
         <RelatedProductsSection />
