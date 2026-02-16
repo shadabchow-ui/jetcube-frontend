88b57c57d427c7822024c9bd5e98ff2e845020a1
shadabchow-ui <shadabchow@gmail.com>
2026-02-16 02:55:34 -0500

    Update ProductComparison.tsx

--- src/screens/ProductComparison/ProductComparison.tsx
+++ src/screens/ProductComparison/ProductComparison.tsx
@@ -1,8 +1,26 @@
 // src/screens/ProductComparison/ProductComparison.tsx
-import NavigationSection from "../Shop/sections/NavigationSection";
-import { ProductComparisonSection } from "./sections/ProductComparisonSection";
-import { QualityAssuranceSection } from "./sections/QualityAssuranceSection";
-import { RelatedProductsSection } from "../SingleProduct/sections/RelatedProductsSection";
+
+import * as NavigationSectionMod from "../Shop/sections/NavigationSection";
+import * as ProductComparisonSectionMod from "./sections/ProductComparisonSection";
+import * as QualityAssuranceSectionMod from "../CartSidebar/sections/QualityAssuranceSection";
+import * as RelatedProductsSectionMod from "../SingleProduct/sections/RelatedProductsSection";
+
+// Support either default-export OR named-export without changing UI/layout
+const NavigationSection =
+  (NavigationSectionMod as any).default ??
+  (NavigationSectionMod as any).NavigationSection;
+
+const ProductComparisonSection =
+  (ProductComparisonSectionMod as any).ProductComparisonSection ??
+  (ProductComparisonSectionMod as any).default;
+
+const QualityAssuranceSection =
+  (QualityAssuranceSectionMod as any).QualityAssuranceSection ??
+  (QualityAssuranceSectionMod as any).default;
+
+const RelatedProductsSection =
+  (RelatedProductsSectionMod as any).RelatedProductsSection ??
+  (RelatedProductsSectionMod as any).default;
 
 export const ProductComparison = (): JSX.Element => {
   return (
